3a21a7fe20eff7a014cb3b1a31609a0b3cc2f460
Derek Wan <d.wan@icloud.com>
2022-10-04 10:49:46 +0800

    Move main logic into `src/`

--- main.py
+++ main.py
@@ -1,28 +1,10 @@
-from typing import Any  # noqa: INP001
-
-from fastapi import FastAPI
-from numpy import random
+import sys  # noqa: INP001
 
 
-app = FastAPI()
+sys.path.append("src")
 
 
-@app.get("/")
-def read_root() -> dict[str, Any]:
-    return {"Hello": "World"}
+from deta_fastapi.app import get_app  # isort: skip  # noqa
 
 
-@app.get("/items/all")
-def read_all_items() -> list[dict[str, Any]]:
-    return [{"Hello": "World"}, {"Bye": "World"}]
-
-
-@app.get("/items/random")
-def read_random_item() -> dict[str, Any]:
-    id = random.randint(low=0, high=10)
-    return {"item_id": id}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int) -> dict[str, Any]:
-    return {"item_id": item_id}
+app = get_app()
