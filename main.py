ee30ffe1b26db6bbd33c0deb192cd80fdaf70926
Derek Wan <d.wan@icloud.com>
2022-10-04 09:19:29 +0800

    Setup app

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from typing import Any  # noqa: INP001
+
 from fastapi import FastAPI
 
 
@@ -5,10 +7,10 @@ app = FastAPI()
 
 
 @app.get("/")
-def read_root():
+def read_root() -> dict[str, Any]:
     return {"Hello": "World"}
 
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int):
+def read_item(item_id: int) -> dict[str, Any]:
     return {"item_id": item_id}
