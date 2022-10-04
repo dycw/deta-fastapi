4bf5a9df2ef41ea5dde033ddef1bc8049eb90ff8
Derek Wan <d.wan@icloud.com>
2022-10-04 10:44:12 +0800

    Add `numpy` as a dependency

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from typing import Any  # noqa: INP001
 
 from fastapi import FastAPI
+from numpy import random
 
 
 app = FastAPI()
@@ -14,6 +15,12 @@ def read_root() -> dict[str, Any]:
 @app.get("/items/all")
 def read_all_items() -> list[dict[str, Any]]:
     return [{"Hello": "World"}, {"Bye": "World"}]
+
+
+@app.get("/items/random")
+def read_random_item() -> dict[str, Any]:
+    id = random.randint(low=0, high=10)
+    return {"item_id": id}
 
 
 @app.get("/items/{item_id}")
